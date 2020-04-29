13bcb790a9d69e41d13e96201ff04592d3df90bd
Iryna Kalanchuk <irynakalanchuk@Irynas-MacBook-Pro.local>
2020-04-29 23:39:04 +0300

    Added hw about Exceptions

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -6,42 +6,49 @@ import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-  /**
-   * Test data for positive test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "data")
-  public static Object[][] data() {
-    return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for positive test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "data")
+    public static Object[][] data() {
+        return new Object[][]{
+                {2, 2, 4},
+                {2, 3, 6},
+                {12, 12, 144},
+                {0, 3, 0}
+        };
+    }
 
-  /**
-   * Test data for exception test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "negativeData")
-  public static Object[][] negativeData() {
-    return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for exception test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "negativeData")
+    public static Object[][] negativeData() {
+        return new Object[][]{
+                {-2, 2},
+                {2, -2},
+                {0, -2},
+                {-4, -5},
+        };
+    }
 
-  @Test(dataProvider = "data")
-  public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "data")
+    public void testRectangleArea(int a, int b, int c) {
+        assertEquals(ExampleException.rectangleArea(a, b), c);
+    }
 
 
-  @Test(dataProvider = "negativeData")
-  public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "negativeData")
+    public void testRectangleAreaNegative(int a, int b) {
+        try {
+            ExampleException.rectangleArea(a, b);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "input value is below zero!");
+        }
+    }
 }
